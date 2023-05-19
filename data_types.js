53fa16a70bd08d49e5c56d1c7ffc8206c9739669
loredanat219 <108115155+loredanat219@users.noreply.github.com>
2023-05-19 15:33:44 +0300

    Add files via upload

---
+++ data_types.js
@@ -0,0 +1,31 @@
+
+//Numbers 
+
+var num = 24;
+let numar = 45;
+const numero = 56;
+let a = 1;
+
+// String
+
+let color = "rosu";
+let culoare = "1";
+
+//Booleans 
+
+let x = true;
+let b = false;
+
+//examples 
+
+let y = 16 + "volvo";
+
+//Obiecte
+
+const person = { firstName: "John", lastName: "Thomas", address: "Gh lazar" };
+
+//Arrays
+
+const cars = ["dacia", "Volvo", "BMW"];
+
+const fruits = ["mere", "pere", "nuci"];
---
+++ ex.js
@@ -0,0 +1,13 @@
+function ageToDays(age){
+    return age * 361
+}
+
+console.log(ageToDays(39));
+
+const aniInzile = (ani) => ani * 361;
+let solution =aniInzile(3);
+console.log(solution);
+
+const cube = (a) => a * a * a;
+let rez = cube(3);
+console.log(rez);
