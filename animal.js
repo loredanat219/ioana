1cb2c6b3372c5328040876e5ff64d42d413750a0
ioana-muntean <134063069+ioana-muntean@users.noreply.github.com>
2023-05-22 11:18:50 +0300

    Add files via upload

---
+++ animal.js
@@ -0,0 +1,12 @@
+let animal = {
+    type: "cat",
+    name: "Tazz",
+    color: "black",
+    legs: 4,
+};
+console.log(animal)
+
+console.log(animal.color)
+
+animal.type = "dog"
+console.log(animal)
